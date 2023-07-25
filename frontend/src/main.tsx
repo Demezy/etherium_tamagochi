9d3f894b5df01bed875f3af1ac794599cfbb4460
Demezy <38487319+Demezy@users.noreply.github.com>
2023-07-25 12:28:23 +0300

    feat: add context for ledger

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import { WagmiConfig } from "wagmi";
+import ledgerConfig from "./ledger/ledger_wrapper";
+import '@fontsource/public-sans';
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router} />
+    <WagmiConfig config={ledgerConfig}>
+      <RouterProvider router={router} />
+    </WagmiConfig>
   </React.StrictMode>,
 );
