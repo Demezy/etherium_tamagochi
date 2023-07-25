a854674e7c2df9dfdbf2a55bc7048e0c21e0ea2c
Alexandr Ragulin <a.ragulin@tinkoff.ru>
2023-07-25 15:39:07 +0300

    feat: add deployment config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,9 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades"
+import "@openzeppelin/hardhat-upgrades";
+
+require('dotenv').config()
+
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+if (ALCHEMY_API_KEY == undefined){
+  throw "Alchemy api key is not provided";
+}
+
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+if (MUMBAI_PRIVATE_KEY == undefined){
+  throw "Mumbai private key is not provided";
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MUMBAI_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
