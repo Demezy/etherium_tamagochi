71cebf22839932caae7381fbca666926397bd774
Demezy <38487319+Demezy@users.noreply.github.com>
2023-07-25 12:29:23 +0300

    refactor: replace links with uikit

--- frontend/src/home_page/home_page.tsx
+++ frontend/src/home_page/home_page.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 // import viteLogo from '/vite.svg'
 import "./home_page.css";
+import { Stack } from "@mui/material";
 
 function HomePage() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1> Etherium tamagochi home page</h1>
+      <h1>Etherium tamagochi home page</h1>
       <div>
-        <Link to="/pet_stats"> to destination page</Link>
+        <Stack>
+          <Link to="/pet_stats">pet page</Link>
+        </Stack>
       </div>
     </>
   );
